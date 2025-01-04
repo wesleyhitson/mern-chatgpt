068c3ff65f1a29f60f83ba2897b3eb20edb21c64
Wesley Hitson <wesleyhitson106@gmail.com>
2025-01-04 11:30:01 -0500

    fix main.tsx imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.tsx'
 import { createTheme, ThemeProvider } from '@mui/material'
