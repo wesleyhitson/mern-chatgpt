6e9671265dbce473c3a5c3a6ee8f80cd89c0fdee
wesleyhitson <wesleyhitson106@gmail.com>
2025-01-23 23:42:41 -0500

    finish chat backend

--- backend/src/controllers/chat-controller.ts
+++ backend/src/controllers/chat-controller.ts
@@ -9,23 +9,30 @@ export const generateChatCompletion = async (
     next: NextFunction
 ) => {
     const { prompt } = req.body;
-    const user = await User.findById(res.locals.jwtData.id);
-    if (!user) {
-        return res
-        .status(401)
-        .json({ message: "User not registered or token malfunctioned" });
+    try {
+        const user = await User.findById(res.locals.jwtData.id);
+        if (!user) {
+            return res
+            .status(401)
+            .json({ message: "User not registered or token malfunctioned" });
+        }
+        // get all user chats
+        const chats = user.chats.map(({role, content}) => ({role, content})) as ChatCompletionRequestMessage[];
+        chats.push({content: prompt, role: "user"});
+        user.chats.push({conten: prompt, role: "user"});
+        // send all chats with new one to API
+        const config = configureOpenAI();
+        const openai = new OpenAIApi(config);
+        const chatResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo", 
+            messages: chats
+        });
+        user.chats.push(chatResponse.data.choices[0].message);
+        await user.save();
+        return res.status(200).json({ chats: user.chats });
+        // get response
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Something went wrong" });
     }
-    // get all user chats
-    const chats = user.chats.map(({role, content}) => ({role, content})) as ChatCompletionRequestMessage[];
-    chats.push({content: prompt, role: "user"});
-    user.chats.push({conten: prompt, role: "user"});
-    // send all chats with new one to API
-    const config = configureOpenAI();
-    const openai = new OpenAIApi(config);
-    const chatResponse = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo", 
-        messages: chats
-    });
-    // get response
-    
 };
