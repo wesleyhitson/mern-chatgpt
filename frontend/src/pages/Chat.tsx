58c2404ffb1f4ffb7f73d2d520e83f6eb50af366
wesleyhitson <wesleyhitson106@gmail.com>
2025-01-27 23:45:17 -0500

    chat page progress

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,7 +1,8 @@
-import { Box } from '@mui/material';
+import { Box, Avatar } from '@mui/material';
 import React from 'react';
 
 const Chat = () => {
+    const auth = 
     return (
         <Box
             sx={{
@@ -11,7 +12,29 @@ const Chat = () => {
                 width: "100%",
                 mt: 3,
                 gap: 3,
-            }}></Box>
+            }}>
+                <Box sx={{ display: {md: "flex", xs: "none", sm: "none"} }}>
+                    <Box sx={{ 
+                        display: "flex", 
+                        width: "100%", 
+                        height: "60vh", 
+                        bgcolor: "rgb(17,29,39)",
+                        borderRadius: 5,
+                        flexDirection: "column",
+                        mx: 3,
+                    }}> 
+                        <Avatar sx={{
+                            mx: "auto",
+                            my: 2,
+                            bgcolor: "white",
+                            color: "black",
+                            fontWeight: 700
+                        }}>
+
+                        </Avatar>
+                    </Box>
+                </Box>
+            </Box>
     );
 };
 
