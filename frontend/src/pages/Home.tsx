7e1fcb34dd031004c060030fb291c7926c837dc9
wesleyhitson <wesleyhitson106@gmail.com>
2025-02-25 16:58:51 -0500

    add size-based styling for mobile

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
 import TypingAnim from "../components/typer/TypingAnim";
 
 const Home = () => {
+    const theme = useTheme();
+    const isBelowMd = useMediaQuery(theme.breakpoints.down("md"));
     return <Box width={"100%"} height={"100%"}>
         <Box 
         sx={{
@@ -47,7 +49,7 @@ const Home = () => {
                     style={{ 
                         display: "flex", 
                         margin: "auto", 
-                        width: "60%", 
+                        width: isBelowMd ? "80%" : "60%", 
                         borderRadius: 20,
                         boxShadow: "-5px -5px 105px #64f3d5",
                         marginTop: 20,
